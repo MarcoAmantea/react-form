622851e13eb6ba79cc8e268530fe606aec89d982
MarcoAmantea <marcoamantea18@gmail.com>
2024-12-17 19:53:02 +0100

    refactor: added image to new article

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }
   const deletePost = (id) => {
     const updatedPosts = post.filter((curPost) => curPost.id !== id);
-    setPost(updatedPosts); 
+    setPost(updatedPosts);
   };
 
   return (
@@ -25,21 +25,23 @@ function App() {
         <form action="" onSubmit={sendPost}>
           <div className="mb-3">
             <div className="text-center"><label htmlFor="articleName" className="form-label"><h1 className="text-white mt-4">Crea e salva il tuo nuovo articolo</h1></label></div>
-            <input type="text" className="form-control" value={newPost} onChange={(event) => setNewPost(event.target.value)} />
+            <textarea className="form-control" value={newPost} onChange={(event) => setNewPost(event.target.value)} />
             <div className="text-center mt-4"><button type="submit" className="btn btn-light">🖊️ Salva articolo 🖊️</button></div>
           </div>
         </form>
       </div>
       {(post.length > 0) ? post.map(curPost => <div key={curPost.id}>
+        <h3 className="text-center text-white">ASCOLTATE TUTTI! ECCO IL TUO NUOVO ARTICOLO!</h3>
+        <div className="text-center mb-3"><img src="../public/wp-16278334959737740266667711549875.jpg" alt="" /></div>
         <div class="card container">
-          <div class="card-body">
+          <div class="card-body text-center">
             {curPost.title}
           </div>
-          
         </div>
-       <div className="container text-center"> <button className="btn btn-danger mt-2" onClick={() => deletePost(curPost.id)}>🗑️ Elimina Articolo 🗑️</button></div>
+        <div className="container text-center"> <button className="btn btn-danger mt-2" onClick={() => deletePost(curPost.id)}>🗑️ Elimina Articolo 🗑️</button></div>
       </div >
       ) : <p className="text-center text-white">Non ci sono articoli predefiniti! Prova a cercarli o a crearli con la barra apposita </p>}
+
     </>
   )
 }
